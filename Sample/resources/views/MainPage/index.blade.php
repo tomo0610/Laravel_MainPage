08dae77ba83be2239dbba51bfdb87e6a3075970d
tomo0610 <tomochiki0610@gmail.com>
2020-05-25 10:23:54 +0900

    googleURLインデックス登録

--- Sample/resources/views/MainPage/index.blade.php
+++ Sample/resources/views/MainPage/index.blade.php
@@ -15,6 +15,7 @@
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
+<meta name="google-site-verification" content="DP1sv-lpxSfx5xdaLWXsHAWGqYWIuSUGwzCqz8fjrUw" />
 </head>
 
 <body>
